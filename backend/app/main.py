63bbab4fbec64e046b74aa81c0cd9991a1fc9685
Alexistodj124 <alexistodj@gmail.com>
2025-03-04 13:24:50 -0600

    Inicio Backend main.py

---
+++ backend/app/main.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+from app.database import neo4j_conn
+from app.routes import nodes, relationships, queries, upload
+
+# Inicializar Flask
+app = Flask(__name__)
+CORS(app)  # Habilitar CORS para todas las rutas
+
+# Registrar Blueprints (rutas)
+app.register_blueprint(nodes.bp, url_prefix="/nodes")
+app.register_blueprint(relationships.bp, url_prefix="/relations")
+app.register_blueprint(queries.bp, url_prefix="/queries")
+app.register_blueprint(upload.bp, url_prefix="/upload")
+
+# Ruta de prueba
+@app.route("/")
+def home():
+    return jsonify({"message": "Backend de Neo4j con Flask está corriendo 🚀"})
+
+# Ejecutar el servidor
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
